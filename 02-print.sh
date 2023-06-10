06bc1969ec1259de08a32b872b458318af03eed0
sairam <sairm21@gmail.com>
2023-06-10 17:48:34 +0530

    correcting  all colour codes to echo

--- 02-print.sh
+++ 02-print.sh
@@ -10,8 +10,8 @@ echo welcome to shell script
 # COL stands for possible colours like Red(31), Green(32), Yellow(33), Blue(34), Magenta(35), Cyan(36)
 
 echo -e "\e[31m print in Red colour\e[0m"
-echo -e "\e32m print in Green colour\e[0m"
-echo -e "\e33m print in Yellow colour\e[0m"
-echo -e "\e34m print in Bluecolour\e[0m"
-echo -e "\e35m print in Magenta colour\e[0m"
-echo -e "\e36m print in Cyancolour\e[0m"
+echo -e "\e[32m print in Green colour\e[0m"
+echo -e "\e[33m print in Yellow colour\e[0m"
+echo -e "\e[34m print in Bluecolour\e[0m"
+echo -e "\e[35m print in Magenta colour\e[0m"
+echo -e "\e[36m print in Cyancolour\e[0m"
